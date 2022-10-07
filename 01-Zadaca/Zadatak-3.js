cbc65b2ba7a1813d1be6529f0c79bb353e3055c4
LukaBrodaric <brodaricluka81@gmail.com>
2022-10-07 17:54:32 +0200

    Riješen treći zadatak

---
+++ 01-Zadaca/Zadatak-3.js
@@ -0,0 +1,8 @@
+
+    var Xuvrijeme = (broj) => {
+         var sati = (broj / 60);
+         var minute = (broj % 60);
+          console.log(`Ovo je ${sati} sata i ${minute} minuta`);
+    };
+
+    Xuvrijeme(120);
