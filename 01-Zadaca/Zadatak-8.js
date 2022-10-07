d0462fba0f60dfd6ff4809ff1c4bbf09da849fa0
LukaBrodaric <brodaricluka81@gmail.com>
2022-10-07 19:13:52 +0200

    Ispravljen osmi zadatak

--- 01-Zadaca/Zadatak-8.js
+++ 01-Zadaca/Zadatak-8.js
@@ -1,10 +1,9 @@
 
     var DvaObjekta = (objekt1, objekt2) => {
 
-        polje1 = JSON.stringify(Object.keys(objekt1));
-        polje2 = JSON.stringify(Object.keys(objekt2));
+      console.log( JSON.stringify(Object.keys(objekt1)) === JSON.stringify(Object.keys(objekt2)) );
         
-        console.log(polje1 === polje2);
+        
 
     };
 
