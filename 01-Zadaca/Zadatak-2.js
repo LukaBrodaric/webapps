33ae3aa45f7ba14efaead1e94a3a43cad0d4efd8
LukaBrodaric <brodaricluka81@gmail.com>
2022-10-07 17:45:52 +0200

    Riješen drugi zadatak

---
+++ 01-Zadaca/Zadatak-2.js
@@ -0,0 +1,13 @@
+
+    var Provjera = (broj) => {
+        if (broj >= 100 && broj <= 150000) {
+            console.log((broj)," je unutar ", [100, 150000]);
+    } else {
+        console.log((broj)," nije unutar ", [100, 150000]);
+    }
+
+};
+
+
+
+Provjera(53);
