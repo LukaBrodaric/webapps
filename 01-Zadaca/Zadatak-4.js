62312e3f54d9f08e0524e4410f5793547b44476a
LukaBrodaric <brodaricluka81@gmail.com>
2022-10-07 18:00:46 +0200

    Riješen cetvrti zadatak

---
+++ 01-Zadaca/Zadatak-4.js
@@ -0,0 +1,8 @@
+
+    const Dijeljenjes3 = (arr) => {
+        polje = arr.filter((x) => x % 3 === 0);
+        console.log(`Brojevi djeljivi s 3: ${polje} `);
+
+    };
+
+    Dijeljenjes3([1,2,3,4,5,6,7,8,9,10]);
